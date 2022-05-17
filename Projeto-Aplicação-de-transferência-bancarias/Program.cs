4cdb61b8c3812933f494673f2e89a75634998cd3
Fábio-José-Américo.Santana <f.americo.santana@gmail.com>
2022-05-17 13:20:43 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto-Aplicação-de-transferência-bancarias/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_Aplicação_de_transferência_bancarias_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
